ca91ba852ee26fd311e9127a3fbe25862fc66517
Anil <p.anilkumar46@gmail.com>
2026-05-12 22:09:53 +0530

    changes

--- dags/dataproc_transformation_dag.py
+++ dags/dataproc_transformation_dag.py
@@ -19,6 +19,12 @@ CLUSTER_CONFIG = {
     "worker_config": {
         "num_instances": 0
     },
+    # --- ADD THIS BLOCK ---
+    "software_config": {
+        "properties": {
+            "core:fs.gs.input.stream.support.content.encoding.gzip": "true"
+        }
+    },
     "gce_cluster_config": {
         "service_account": "dataproc-airflow-sa-prod@ecommerce-de-project-495511.iam.gserviceaccount.com"
     }
