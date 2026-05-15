cf59afef348286808ae7f8228c3db8a15b411dde
Anil <p.anilkumar46@gmail.com>
2026-05-15 18:52:45 +0530

    changes

--- scripts/transform_job_to_silver.py
+++ scripts/transform_job_to_silver.py
@@ -55,7 +55,9 @@ def process_bronze_to_silver(spark):
     for table in tables_to_process:
         print(f"--- Processing {table} ---")
         # Read Bronze
-        raw_df = spark.read.format("parquet").load(f"gs://olist-bronze-prod-lake/{table}/")
+        raw_df = spark.read.parquet(
+    f"gs://olist-bronze-prod-lake/{table}/{table}_raw_data_2026-05-15.parquet"
+    )
         # Run DQ Check
         clean_df, bad_df = apply_dq_rules(raw_df, table)
 
